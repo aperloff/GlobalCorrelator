d5101cac7fc23523361e3d1526b87c5c09ea85d7
Noah Zipper <noah.zipper@colorado.edu>
2021-06-15 11:24:41 -0600

    added different bitstring tests

--- pulsar_devkit/simple_examples/HLSIPs/simple_algo_apfixed_logic_test.cpp
+++ pulsar_devkit/simple_examples/HLSIPs/simple_algo_apfixed_logic_test.cpp
@@ -1,32 +1,44 @@
 #include <cstdio>
+#include <iostream>
 #include "src/simple_algo_apfixed_logic.h"
 #include "ap_int.h"
 #include "ap_fixed.h"
 
-#define NTEST 1
-
-
 int main() {
 	
-	ap_uint<12> inA;
-	ap_fixed<12, 5, AP_RND_CONV, AP_SAT> inB;
-	ap_uint<1> outA;
-	ap_uint<1> outB;
+	unsigned int n_inputs = 6;
+	ap_uint<12> int_inputs[6] = {0b000000000000, 0b000000000001, 0b111111111111, 0b111111111110, 0b100000000000, 0b011111111111};
+	ap_fixed<12, 5, AP_RND_CONV, AP_SAT> fixed_inputs[6] = {0b000000000000, 0b000000000001, 0b111111111111, 0b111111111110, 0b100000000000, 0b011111111111};
+	
+	for (unsigned int input = 0; input < n_inputs; ++input) {
+		std::cout << "\nRaw Input (ap_uint Format)= " << int_inputs[input].to_string().c_str() << std::endl;
+		std::cout << "Raw Input (ap_fixed Format)= " << fixed_inputs[input].to_string().c_str() << std::endl;
+		ap_uint<12> inA = int_inputs[input];
+		ap_fixed<12, 5, AP_RND_CONV, AP_SAT> inB = fixed_inputs[input];
+		ap_uint<1> outA_ref = 0;
+		ap_uint<1> outB_ref = 0;
+		ap_uint<1> outA_hw = 0;
+		ap_uint<1> outB_hw = 0;
 
-	for (int test = 1; test <= NTEST; ++test) {
+		simple_algo_apfixed_logic_ref(inA, inB, outA_ref, outB_ref);
+		simple_algo_apfixed_logic_hw(inA, inB, outA_hw, outB_hw);
 
-		inA = 1;
-		inB = 1;
-		outA = 0;
-		outB = 0;
+		std::cout << "\tC Code Ref: \n\t\tap_uint input " << inA << " -> ap_uint output (!input) " << outA_ref << " \n\t\tap_fixed input " << inB << " -> ap_uint output (!input) " << outB_ref << std::endl;
+		if (outA_ref != outB_ref) {
+			printf( "C Code Reference Data Type Discrepency!" );
+		}
+		std::cout << "\tHW: \n\t\tap_uint input " << inA << " -> ap_uint output (!input) " << outA_hw << " \n\t\tap_fixed input " << inB << " -> ap_uint output (!input) " << outB_hw << std::endl;
+		if (outA_hw != outB_hw) {
+			printf( "HW Data Type Discrepency!" );
+		}
 
-		simple_algo_apfixed_logic_ref(inA, inB, outA, outB);
-		printf( "ref test: \n\tap_uint in %i -> ap_uint !in %i \n\tap_fixed in %f -> ap_uint !in %i\n", int(inA), int(outA), float(inB), int(outB) );
-		
-		simple_algo_apfixed_logic_hw(inA, inB, outA, outB);
-		printf( "hw test: \n\tap_uint in %i -> ap_uint !in %i \n\tap_fixed in %f -> ap_uint !in %i\n", int(inA), int(outA), float(inB), int(outB) );
+		if (outA_ref != outA_hw) {
+			printf( "ap_uint Ref-HW Discrepency!!!" );
+		}
 
-
+		if (outB_ref != outB_hw) {
+			printf( "ap_fixed Ref-HW Discrepency!!!" );
+		}
 	}
 
 	return 0;
