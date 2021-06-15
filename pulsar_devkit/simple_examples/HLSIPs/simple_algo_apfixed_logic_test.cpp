10401498063f7dc8dcfc4324861552a7f2b78c41
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2021-06-15 15:32:32 -0600

    Fix the pit patters for the ap_fixed values.

--- pulsar_devkit/simple_examples/HLSIPs/simple_algo_apfixed_logic_test.cpp
+++ pulsar_devkit/simple_examples/HLSIPs/simple_algo_apfixed_logic_test.cpp
@@ -1,20 +1,19 @@
 #include <cstdio>
 #include <iostream>
+#include <vector>
 #include "src/simple_algo_apfixed_logic.h"
 #include "ap_int.h"
 #include "ap_fixed.h"
 
 int main() {
 	
-	unsigned int n_inputs = 6;
-	ap_uint<12> int_inputs[6] = {0b000000000000, 0b000000000001, 0b111111111111, 0b111111111110, 0b100000000000, 0b011111111111};
-	ap_fixed<12, 5, AP_RND_CONV, AP_SAT> fixed_inputs[6] = {0b000000000000, 0b000000000001, 0b111111111111, 0b111111111110, 0b100000000000, 0b011111111111};
+   std::vector<ap_uint<12>> int_inputs = {0b000000000000, 0b000000000001, 0b111111111111, 0b111111111110, 0b100000000000, 0b011111111111, 0b000010000000, 0b111101111111};
 	
-	for (unsigned int input = 0; input < n_inputs; ++input) {
+    for (unsigned int input = 0; input < int_inputs.size(); ++input) {
 		std::cout << "\nRaw Input (ap_uint Format)= " << int_inputs[input].to_string().c_str() << std::endl;
-		std::cout << "Raw Input (ap_fixed Format)= " << fixed_inputs[input].to_string().c_str() << std::endl;
 		ap_uint<12> inA = int_inputs[input];
-		ap_fixed<12, 5, AP_RND_CONV, AP_SAT> inB = fixed_inputs[input];
+		ap_fixed<12, 5, AP_RND_CONV, AP_SAT> inB = 0;
+        inB.V = int_inputs[input](11,0);
 		ap_uint<1> outA_ref = 0;
 		ap_uint<1> outB_ref = 0;
 		ap_uint<1> outA_hw = 0;
